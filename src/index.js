510b6dcb2c7da5b0274858e77397d0b9d02338de
Maria Buznik <masha.vinogradova82@gmail.com>
2022-08-16 12:59:40 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -9,66 +9,60 @@ const inpText = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 
-const clearContent = ref => (ref.innerHTML = '');
+const clearContent = () => {
+  countryInfo.innerHTML = '';
+  countryList.innerHTML = '';
+};
 
 const inputHandler = e => {
   const textInp = e.target.value.trim();
   if (!textInp) {
-    clearContent(countryList);
-    clearContent(countryInfo);
+    clearContent();
     return;
   }
 
   fetchCountries(textInp)
     .then(country => {
+      clearContent();
+
       if (country.length > 10) {
         Notiflix.Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
         return;
+      } else if (country.length === 1) {
+        createInfoMarkup(country);
+      } else if (country.length > 1 && country.length <= 10) {
+        createListMarkup(country);
       }
-      renderMarkup(country);
     })
     .catch(error => {
-      clearContent(countryList);
-      clearContent(countryInfo);
       Notiflix.Notify.failure('Oops, there is no country with that name');
+      clearContent();
     });
 };
 
-const renderMarkup = country => {
-  if (country.length === 1) {
-    clearContent(countryList);
-    const markupInfo = createInfoMarkup(country);
-    countryInfo.innerHTML = markupInfo;
-  } else {
-    clearContent(countryInfo);
-    const markupList = createListMarkup(country);
-    countryInfo.innerHTML = markupList;
-  }
-};
-
 const createListMarkup = country => {
-  return country
-    .map(
-      ({ name, flags }) =>
-        `<li><img src="${flags.png}" alt="${name.official}" width="40" height="20">${name.official}</li>`
-    )
+  const markup = country
+    .map(({ name, flags }) => {
+      return `<li><img src="${flags.svg}" alt="${name.official}" width="40" height="20"><p>${name.official}</p></li>`;
+    })
     .join('');
+  countryList.innerHTML = markup;
 };
 
 const createInfoMarkup = country => {
-  return country
-    .map(
-      ({ name, capital, population, flags, languages }) =>
-        `<h1><img src="${flags.png}" alt="${
-          name.official
-        }" width="90" height="70">${name.official}</h1>
+  const markup = country
+    .map(({ name, capital, population, flags, languages }) => {
+      return `<section><h1><img src="${flags.png}" alt="${
+        name.official
+      }" width="90" height="70">${name.official}</h1>
       <p>Capital: ${capital}</p>
       <p>Population: ${population}</p>
-      <p>Languages: ${Object.values(languages)}</p>`
-    )
+      <p>Languages: ${Object.values(languages)}</p><section>`;
+    })
     .join('');
+  countryInfo.innerHTML = markup;
 };
 
 inpText.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
